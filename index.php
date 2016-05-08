d5297702ba02bdf1f14ecee971734d93523a2651
elpolakopolania <gustav0796@hotmail.com>
2016-05-08 11:34:52 -0500

    Título

--- index.php
+++ index.php
@@ -26,7 +26,7 @@
         <div class="section no-pad-bot" id="index-banner">
             <div class="container">
                 <br><br>
-                <h1 class="header center orange-text">Software académico</h1>
+                <h1 class="header center orange-text">Software académico.</h1>
                 <br><br>
             </div>
         </div>
